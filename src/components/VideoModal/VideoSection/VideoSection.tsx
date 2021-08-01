916a9802626286fa5d45a6c780df240515e5d2f7
hanbin9775 <dnffkrktyd@gmail.com>
2021-08-01 15:02:07 +0900

    [#3] add: mp4 module type added in react-app-env.d.ts

--- src/components/VideoModal/VideoSection/VideoSection.tsx
+++ src/components/VideoModal/VideoSection/VideoSection.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import styled, { ThemeContext } from 'styled-components';
+import SampleVideo from 'assets/video/shorts_1min.mp4';
 
 const VideoSection = () => {
   return <div>Hi</div>;
