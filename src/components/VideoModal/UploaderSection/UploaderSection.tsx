2695dafd9d7e65f8e81ffdc5edf9f929c3765c6b
hanbin9775 <dnffkrktyd@gmail.com>
2021-08-01 23:33:19 +0900

    [#3] feat: VideoModal-UploaderSection UI implemented

--- src/components/VideoModal/UploaderSection/UploaderSection.tsx
+++ src/components/VideoModal/UploaderSection/UploaderSection.tsx
@@ -3,7 +3,75 @@ import styled, { ThemeContext } from 'styled-components';
 
 const UploaderSection = () => {
   const themeStyle = useContext(ThemeContext);
-  return <div>Hi</div>;
+  return (
+    <ContentWrapper gray={themeStyle.color.grayScale[250]}>
+      <ProfileWrapper>
+        <ProfileImage />
+        <ProfileText>
+          <ProfileName>lolking_123</ProfileName>
+          <ProfileFollow gray={themeStyle.color.grayScale[500]}>
+            팔로우 123
+          </ProfileFollow>
+        </ProfileText>
+        <FollowButton mainColor={themeStyle.color.yellow}>팔로우</FollowButton>
+      </ProfileWrapper>
+      <DescriptionParagraph>
+        레오나로 아칼리 솔킬 #매드무비
+      </DescriptionParagraph>
+    </ContentWrapper>
+  );
 };
+
+const ContentWrapper = styled.div<{ gray: string }>`
+  border-bottom: ${(props) => `1px solid ${props.gray}`};
+  height: 180px;
+`;
+
+const ProfileWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 1rem;
+  margin-bottom: 1.2rem;
+`;
+
+const ProfileImage = styled.div`
+  border-radius: 5px;
+  height: 40px;
+  width: 40px;
+  background: gray;
+`;
+
+const ProfileText = styled.div`
+  width: 225px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProfileName = styled.div`
+  font-weight: 600;
+  font-size: 1.4rem;
+`;
+
+const ProfileFollow = styled.div<{ gray: string }>`
+  color: ${(props) => props.gray};
+`;
+
+const FollowButton = styled.div<{ mainColor: string }>`
+  width: 70px;
+  height: 36px;
+  font-size: 1.4rem;
+  background: ${(props) => props.mainColor};
+  color: white;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+const DescriptionParagraph = styled.p`
+  width: 100%;
+  font-size: 1.2rem;
+`;
 
 export default UploaderSection;
