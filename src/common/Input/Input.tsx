18a0281bbedadcd33af8cb8e744f69526e9d4c7d
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-08-06 21:15:42 +0900

    feat: input components

---
+++ src/common/Input/Input.tsx
@@ -0,0 +1,98 @@
+import React, { ChangeEvent } from 'react';
+import styled from 'styled-components';
+
+type Props = {
+  inputType?: string;
+  width?: string;
+  height?: string;
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeHolder?: string;
+  placeHolderFontSize?: string;
+  borderRadius?: number;
+  borderStyle?: string;
+  backgroundColor: string;
+  placeHolderColor?: string;
+  fontSize: string;
+  paddingStyle: string;
+  fontColor?: string;
+};
+
+interface IInputStyleProps {
+  width: string;
+  height: string;
+  borderRadius?: number;
+  borderStyle?: string;
+  backgroundColor: string;
+  placeHolderColor: string;
+  fontSize: string;
+  paddingStyle: string;
+  placeHolderFontSize: string;
+  fontColor: string;
+}
+
+const Input = ({
+  inputType,
+  width = '100%',
+  height = '1rem',
+  value,
+  onChange,
+  placeHolder = '',
+  borderRadius,
+  borderStyle = 'none',
+  backgroundColor,
+  placeHolderColor = '#000',
+  fontSize,
+  paddingStyle,
+  placeHolderFontSize = '1rem',
+  fontColor = '#000',
+}: Props) => {
+  return (
+    <InputItem
+      type={inputType}
+      value={value}
+      onChange={onChange}
+      width={width}
+      height={height}
+      placeholder={placeHolder}
+      borderRadius={borderRadius}
+      borderStyle={borderStyle}
+      backgroundColor={backgroundColor}
+      placeHolderColor={placeHolderColor}
+      fontSize={fontSize}
+      paddingStyle={paddingStyle}
+      placeHolderFontSize={placeHolderFontSize}
+      fontColor={fontColor}
+    />
+  );
+};
+
+export default Input;
+
+const InputItem = styled.input<IInputStyleProps>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  border-radius: ${({ borderRadius }) => borderRadius};
+  border: ${({ borderStyle }) => borderStyle};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  font-size: ${({ fontSize }) => fontSize};
+  padding: ${({ paddingStyle }) => paddingStyle};
+  color: ${({ fontColor }) => fontColor};
+
+  &::placeholder {
+    color: ${({ placeHolderColor }) => placeHolderColor};
+    font-size: ${({ placeHolderFontSize }) => placeHolderFontSize};
+  }
+`;
+
+Input.defaultProps = {
+  inputType: 'text',
+  width: '100%',
+  height: '100%',
+  borderRadius: 0,
+  borderStyle: 'none',
+  placeHolder: '',
+  placeHolderFontSize: '1rem',
+  placeHolderColor: '#000',
+  fontColor: '#000',
+};
