f1e7446dc1491c2a8e95a41c868e1b231109731c
hanbin9775 <dnffkrktyd@gmail.com>
2021-08-11 23:13:22 +0900

    [#26] feat: file(video) read and render

--- src/components/Upload/FirstContent.tsx
+++ src/components/Upload/FirstContent.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
-import React, { useContext, useState } from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useContext, useState, useEffect } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import UploadFind from 'assets/svg/upload_1.svg';
 import LevelOne from 'assets/svg/upload_level_1.svg';
@@ -14,54 +15,51 @@ type a = {
 
 const FirstContent = () => {
   const themeStyle = useContext(ThemeContext);
-  const [selectedFile, setSelectedFile] = useRecoilState(
-    uploadSelectedFileState
+  const [selectedFile, setSelectedFile] = useState<string | ArrayBuffer | null>(
+    ''
   );
-
-  const onChange = (e: any) => {
-    const file = e.target.files[0];
-    const reader: FileReader = new FileReader();
-    reader.onload = function () {
-        const a = reader.result;
-      return (
-        <video autoPlay muted loop width="100%" height="100%">
-          <source src={a} type="video/mp4" />
-        </video>
-      );
+  const onChange = (event: any) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = function (e) {
+      const fileContent = e?.target?.result;
+      setSelectedFile(fileContent as string);
     };
-//     reader.onloadend = (event: any) => {
-//       const arrayBuffer = event.target.result;
-//       const fileType = 'video/mpeg';
-//       const blob = new Blob(arrayBuffer, { type: fileType });s
-//       const src = URL.createObjectURL(blob);
-//       //   video.src = src;
-//       return (
-//         <video autoPlay muted loop width="100%" height="100%">
-//           <source src={src} type="video/mp4" />
-//         </video>
-//       );
-//     };
-//     reader.readAsArrayBuffer(file);
-//   };
+  };
 
+  useEffect(() => {
+    console.log(selectedFile);
+  }, [selectedFile]);
+
+  //     reader.onloadend = (event: any) => {
+  //       const arrayBuffer = event.target.result;
+  //       const fileType = ‘video/mpeg’;
+  //       const blob = new Blob(arrayBuffer, { type: fileType });s
+  //       const src = URL.createObjectURL(blob);
+  //       //   video.src = src;
+  //       return (
+  //         <video autoPlay muted loop width=“100%” height=“100%“>
+  //           <source src={src} type=“video/mp4” />
+  //         </video>
+  //       );
+  //     };
+  //     reader.readAsArrayBuffer(file);
+  //   };
   // const reader = new FileReader();
-
   // setSelectedFile(file);
-
   // reader.onload = (pe) => {
   //   // eslint-disable-next-line react-hooks/rules-of-hooks
   // //   const [fileContent, setFileContent] = useState(reader.result);
   // //   console.log(fileContent);
-
   //   //   const a = reader.result;
   //   //   setFileContent(reader.result);
   //   return (
-  //     <video autoPlay muted loop width="100%" height="100%">
-  //       <source src={fileContent?.toString()} type="video/mp4" />
+  //     <video autoPlay muted loop width=“100%” height=“100%“>
+  //       <source src={fileContent?.toString()} type=“video/mp4” />
   //     </video>
   //   );
   // };
-
   return (
     <ContentWrapper>
       <label className="input-btn" htmlFor="input_file">
@@ -71,9 +69,14 @@ const FirstContent = () => {
       </label>
       <input type="file" id="input_file" onChange={(e) => onChange(e)} />
       <div>
-        <video autoPlay muted loop width="100%" height="100%">
-          <source src={selectedFile} type="video/mp4" />
-        </video>
+        <video
+          autoPlay
+          muted
+          loop
+          width="50%"
+          height="50%"
+          src={selectedFile as string}
+        />
       </div>
       <BottomWrapper>
         <BottomContent>
@@ -98,17 +101,13 @@ const FirstContent = () => {
     </ContentWrapper>
   );
 };
-
 export default FirstContent;
-
 const ContentWrapper = styled.div``;
-
 const ImgWrapper = styled.div`
   width: fit-content;
   margin: ${({ theme }) => theme.margins.base};
 `;
 const Img = styled.img``;
-
 const BottomWrapper = styled.div`
   display: flex;
   width: 100%;
