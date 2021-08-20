69ee2645ab6fa7b600aaf9cc9819676bf1d52c07
권순관 <48983361+SoonGwan@users.noreply.github.com>
2021-08-20 20:06:59 +0900

    feat: profile component 구조 셋팅

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import GreetingPage from 'pages/GreetingPage';
 import { Route, Switch } from 'react-router-dom';
 import GlobalStyle from 'styles/GlobalStyle';
+import ProfilePage from 'pages/ProfilePage';
 
 const App = () => {
   return (
     <>
       <Switch>
         <Route exact path="/" component={GreetingPage} />
+        <Route exact path="/profile" component={ProfilePage} />
       </Switch>
       <GlobalStyle />
     </>
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Profile = () => {
+  return (
+    <div>
+      <div>Profile</div>
+    </div>
+  );
+};
+
+export default Profile;
