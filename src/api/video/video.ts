2ba45147e77d7e4ccf9511bd0cd510108b43c1d7
gigibean <gigibean@naver.com>
2021-09-02 17:29:05 +0900

    Update video.ts
    update axios

--- src/api/video/video.ts
+++ src/api/video/video.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import customAxios from 'lib/axios';
 
 // eslint-disable-next-line consistent-return
 const getVideos = async () => {
   // FIX: address
-  const response = await axios.get(
+  const response = await customAxios.get(
     `${process.env.REACT_APP_SERVER}/videos/all/middle`
   );
   return response.data.datas;
