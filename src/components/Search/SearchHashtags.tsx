74e0c5414960bd652a28ba4a3dfbc5734febb748
gigibean <gigibean@naver.com>
2021-09-03 17:37:50 +0900

    Create SearchHashtags.tsx

---
+++ src/components/Search/SearchHashtags.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import VideoListMain from 'common/VideoList/Main';
+import VideoSelectBar from 'components/Main/VideoSelectBar';
+import { hListbySelectorState } from 'atom/searchAreaAtom';
+
+const SearchHashtags = () => {
+  const videos = useRecoilValue(hListbySelectorState);
+
+  return (
+    <>
+      <VideoSelectBar />
+      <VideoListMain videos={videos} isNeedDescription />
+    </>
+  );
+};
+
+export default SearchHashtags;
